d9bc0b0741fa201afdcc9f0d921f9d4f939023a8
LAB_J205 <72319553@continental.edu.pe>
2025-03-21 13:09:17 -0500

    Que miras SAPO

--- operaciones.py
+++ operaciones.py
@@ -1,4 +1,8 @@
+def suma(sumando1,sumando2):
+    return sumando1+sumando2
+
+
 sumando1 = float(input("Primer sumando: "))
 sumando2 = float(input("Segundo sumando: "))
-suma = sumando1 + sumando2
-print(f"Esta es la suma de {sumando1} y {sumando2} = {suma}")
+
+print(f"Esta es la suma de {sumando1} y {sumando2} = {suma(sumando1,sumando2)}")
